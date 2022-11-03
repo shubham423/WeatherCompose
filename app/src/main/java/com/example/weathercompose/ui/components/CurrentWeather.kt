4dcb27afe0c96304cb71e447f118c27230e84ae6
shubham423 <shubh991172@gmail.com>
2022-11-03 20:48:18 +0530

    header ui

---
+++ app/src/main/java/com/example/weathercompose/ui/components/CurrentWeather.kt
@@ -0,0 +1,73 @@
+package com.example.weathercompose.ui.components
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.weathercompose.R
+import com.example.weathercompose.ui.theme.WeatherComposeTheme
+
+data class CurrentWeather(
+    val location: String,
+    val date: String,
+    @DrawableRes val weatherIcon: Int,
+    val weatherDescription: String,
+)
+
+@Composable
+fun CurrentWeatherField(
+    currentWeather: CurrentWeather,
+    modifier: Modifier = Modifier
+) = Column(modifier = modifier.padding(16.dp)) {
+    Text(
+        text = currentWeather.location,
+        style = MaterialTheme.typography.h5.copy(
+            fontWeight = FontWeight.Medium
+        )
+    )
+    Text(
+        text = currentWeather.date,
+        style = MaterialTheme.typography.subtitle2.copy(
+            fontWeight = FontWeight.Normal
+        )
+    )
+    Icon(
+        painter = painterResource(id = currentWeather.weatherIcon),
+        contentDescription = null,
+        tint = Color.Unspecified
+    )
+    Text(
+        modifier = Modifier.fillMaxWidth(),
+        textAlign = TextAlign.Center,
+        text = currentWeather.weatherDescription,
+        style = MaterialTheme.typography.h4.copy(
+            fontWeight = FontWeight.Medium
+        )
+    )
+}
+
+@Preview(showBackground = true, backgroundColor = 0xfffff)
+@Composable
+fun CurrentWeatherFieldPreview() {
+    WeatherComposeTheme {
+        CurrentWeatherField(
+            currentWeather = CurrentWeather(
+                "New York",
+                "Today, 12 July",
+                R.drawable.ic_sunnycloudy,
+                "Sunny Cloudy"
+            )
+        )
+    }
+}
