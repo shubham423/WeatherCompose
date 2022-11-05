6ecb63a2c125fb3aadcffd60835c933dc77c00a3
shubham423 <shubh991172@gmail.com>
2022-11-05 20:34:17 +0530

    custom colors for night mode

--- app/src/main/java/com/example/weathercompose/ui/components/CurrentWeather.kt
+++ app/src/main/java/com/example/weathercompose/ui/components/CurrentWeather.kt
@@ -1,13 +1,16 @@
 package com.example.weathercompose.ui.components
 
 import androidx.annotation.DrawableRes
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Icon
+import androidx.compose.material.LocalContentColor
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -16,6 +19,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.weathercompose.R
+import com.example.weathercompose.ui.theme.Black
 import com.example.weathercompose.ui.theme.WeatherComposeTheme
 
 data class CurrentWeather(
@@ -30,34 +34,45 @@ fun CurrentWeatherField(
     currentWeather: CurrentWeather,
     modifier: Modifier = Modifier
 ) = Column(modifier = modifier.padding(16.dp)) {
-    Text(
-        text = currentWeather.location,
-        style = MaterialTheme.typography.h5.copy(
-            fontWeight = FontWeight.Medium
+
+    val textColor = if (isSystemInDarkTheme()) {
+        Color.White
+    } else {
+        Black
+    }
+
+    CompositionLocalProvider(LocalContentColor provides textColor) {
+
+        Text(
+            text = currentWeather.location,
+            style = MaterialTheme.typography.h5.copy(
+                fontWeight = FontWeight.Medium
+            )
         )
-    )
-    Text(
-        text = currentWeather.date,
-        style = MaterialTheme.typography.subtitle2.copy(
-            fontWeight = FontWeight.Normal
+        Text(
+            text = currentWeather.date,
+            style = MaterialTheme.typography.subtitle2.copy(
+                fontWeight = FontWeight.Normal
+            )
         )
-    )
-    Icon(
-        painter = painterResource(id = currentWeather.weatherIcon),
-        contentDescription = null,
-        tint = Color.Unspecified
-    )
-    Text(
-        modifier = Modifier.fillMaxWidth(),
-        textAlign = TextAlign.Center,
-        text = currentWeather.weatherDescription,
-        style = MaterialTheme.typography.h4.copy(
-            fontWeight = FontWeight.Medium
+        Icon(
+            painter = painterResource(id = currentWeather.weatherIcon),
+            contentDescription = null,
+            tint = Color.Unspecified
         )
-    )
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            text = currentWeather.weatherDescription,
+            style = MaterialTheme.typography.h4.copy(
+                fontWeight = FontWeight.Medium
+            )
+        )
+    }
 }
 
-@Preview(showBackground = true, backgroundColor = 0xfffff)
+@Preview(showBackground = true,
+    backgroundColor = 0xfffff)
 @Composable
 fun CurrentWeatherFieldPreview() {
     WeatherComposeTheme {
